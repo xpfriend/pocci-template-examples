0a11f32f9fa8a92350765a357c05fe3bb3a137e8
ototadana <ototadana@gmail.com>
2016-10-26 21:09:01 +0900

    Add proxy service

---
+++ services/core/proxy/js/proxy.js
@@ -0,0 +1,14 @@
+'use strict';
+
+module.exports = {
+  addDefaults: function(options) {
+    options.proxy         = options.proxy         || {};
+    options.proxy.address = options.proxy.address || '172.20.0.253';
+    options.proxy.url     = options.proxy.url     || 'http://' + options.proxy.address + ':8888';
+  },
+  addEnvironment: function(options, environment) {
+    environment.PROXY_ADDRESS   = options.proxy.address;
+    environment.http_proxy      = options.proxy.url;
+    environment.https_proxy     = options.proxy.url;
+  }
+};
