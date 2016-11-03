7b78d866fc437db9f0dd0ddff4f7996d29875fca
ototadana <ototadana@gmail.com>
2016-11-03 22:23:32 +0900

    Remove Nexus template

--- services/core/nexus/setup-workspace.sh
+++
@@ -1,9 +0,0 @@
-if [ `grep "java:$" ${CONFIG_DIR}/workspaces.yml | wc -l` -eq 0 ]; then
-    return
-fi
-
-if [ `docker ps | grep poccin_java_1 | wc -l` -eq 0 ]; then
-    ${BIN_DIR}/open-workspace
-fi
-
-docker cp ./settings.xml poccin_java_1:/tmp/user_home/.m2/settings.xml
